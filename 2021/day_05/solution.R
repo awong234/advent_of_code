0445e9840ba2ea3d0e604a7a2298ab97409d31db
Alec Wong <aw685@cornell.edu>
2021-12-05 08:14:29 -0500

    Add solution for fifth day

---
+++ 2021/day_05/solution.R
@@ -0,0 +1,71 @@
+checkpoint::checkpoint('2021-11-01')
+
+library(sf)
+library(dplyr)
+library(data.table)
+library(ggplot2)
+
+file = 'input.txt'
+
+cat(file = 'lines.awk',
+'
+BEGIN {
+    OFS=", "
+    FS=" -> "
+}
+{
+    gsub(/,/, " ")
+    $1=$1
+    print "LINESTRING(" $0 ")"
+}
+')
+
+lines = system(sprintf('awk -f lines.awk %s', file), intern = TRUE)
+unlink('lines.awk')
+lines = sf::st_as_sfc(lines)
+
+# View the lines
+ggplot(lines) +
+    geom_sf()
+
+# Part 1
+idx = 1
+straights = list()
+for (i in 1:length(lines)) {
+    if (length(unique(lines[[i]][,2])) == 1 |
+        length(unique(lines[[i]][,1])) == 1) {
+        straights[[idx]] = lines[[i]]
+        idx = idx + 1
+    }
+}
+straights = st_as_sfc(straights)
+ggplot(straights) + geom_sf()
+
+get_points_on_line = function(line) {
+    xdiff = abs(diff(line[[1]][,1]))
+    ydiff = abs(diff(line[[1]][,2]))
+    maxdiff = max(xdiff, ydiff)
+    st_line_sample(line, type = 'regular', sample = seq(0, 1, length = maxdiff+1))
+}
+
+count_intersects = function(lines) {
+    points = list()
+    for (i in seq_along(lines)) {
+        cat("Iter", i, 'of', length(lines), '\r')
+        points[[i]] = get_points_on_line(lines[i])
+    }
+    cat('\n')
+    points = do.call(c, points)
+    coords = st_coordinates(points)
+    coords[] = as.integer(round(coords,0))
+    points = data.table(coords)
+    intersects = points[, .(N=.N), by=c('X', 'Y')]
+    res = sum(intersects[,N] >= 2)
+    message("There are ", res, " intersections")
+    return(res)
+}
+count_intersects(straights)
+
+# Part 2
+count_intersects(lines)
+
