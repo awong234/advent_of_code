0f67b4ddce65eb1d8f6ed03727615a97bcec9b3a
Alec Wong <aw685@cornell.edu>
2021-12-07 21:40:41 -0500

    Fixed bug, made code more robust

--- 2021/day_07/solution.R
+++ 2021/day_07/solution.R
@@ -5,17 +5,17 @@ data = matrix(as.integer(strsplit(x = readLines(file), split = ',')[[1]]), ncol 
 # median(data)  # Answer to part I  suspiciously close to median, could probably just compute median here.
 
 # Part I
-all = sequence(range(data))
+all = seq(range(data)[1], range(data)[2])
 diffs = outer(X = data, Y = all, FUN = function(x,y) abs(x-y))
 diffs = drop(diffs)
 sprintf("Fuel spent in Part I: %d", colSums(diffs)[which.min(colSums(diffs))])
 # all[which.min(colSums(diffs))]
 # Part II
 termial = function(x) {
-    vapply(x, function(x) sum(x:0), FUN.VALUE = c(0L))
+    vapply(x, function(x) sum(seq(x, 0)), FUN.VALUE = c(0L))
 }
 
-termial_lookup = cbind(sequence(range(data)), termial(sequence(range(data))))
+termial_lookup = cbind(all, termial(all))
 diffs = outer(X = data, Y = all, FUN = function(x,y) abs(x-y))
 diffs = drop(diffs)
 diffs[] = termial_lookup[match(x = diffs, table = termial_lookup[,1]) ,2]
