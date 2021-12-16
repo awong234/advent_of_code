bed6255b4e62d146489ed660e2c02102b0843fc9
Alec Wong <aw685@cornell.edu>
2021-12-15 21:24:57 -0500

    Modify day 12 solution

--- 2021/day_12/solution.R
+++ 2021/day_12/solution.R
@@ -1,5 +1,4 @@
 file = 'input_pgr.txt'
-# file = 'input_pgr.txt'
 paths = data.table::fread(cmd = sprintf('awk \'BEGIN{FS="-";OFS=","} {$1=$1;print $0}\' %s', file), header = FALSE)
 names(paths) = c('from', 'to')
 paths_rev = paths
@@ -29,37 +28,28 @@ path_points = function(paths) {
 
 adj = path_points(paths)
 visit = function(mat, from, depth = 0) {
-    # cat(rep("|", depth), from, '\n')
-    dest_options = mat[rownames(mat) == tail(from, 1), ]
+    dest_options = mat[rownames(mat) == from, ]
     dest_options = names(dest_options)[ dest_options > 0 ]
     dest_ind = which(dest_options > 0)
     if (length(dest_options) == 0) {
-        if (tail(from, 1) == 'end') {
-            return(from) # Returns the collected from values
+        if (from == 'end') {
+            return(1)
         } else {
             return(NULL)
         }
-
     }
+    # Recursively apply visit, decrementing from visit counter within each cave path
     out = lapply(dest_options, function(to) {
+        # Wherever we go next, we remove 1 from the visit ticker. Except for big
+        # caves, those have infinite visits. Because the matrix is passed on to
+        # subsequent visits, the tickers are all independent.
         mat[, colnames(mat) == to] = mat[, colnames(mat) == to] - 1
-        from_collect = c(from, to)
-        visit(mat, from_collect, depth = depth + 1)
+        visit(mat, to, depth = depth + 1)
     })
     return(out)
 }
 opts = visit(adj, 'start')
-
-# https://stackoverflow.com/questions/8139677/how-to-flatten-a-list-to-a-list-without-coercion/8139959#8139959
-flatten2 <- function(x) {
-    len <- sum(rapply(x, function(x) 1L))
-    y <- vector('list', len)
-    i <- 0L
-    rapply(x, function(x) { i <<- i+1L; y[[i]] <<- x })
-    y
-}
-
-sum(rapply(opts, function(x) 1L))
+cat("First solution is: ", sum(unlist(opts)), "\n")
 
 # Part II
 
@@ -68,22 +58,22 @@ visit2 = function(mat, from, depth = 0) {
     small_cave_visited = rep(0, length(small_caves))
     names(small_cave_visited) = small_caves
     .visit = function(mat, from, depth = 0, small_cave_visited) {
-        # cat(small_cave_visited, ' ', rep("|", depth), from, '\n')
-        dest_options = mat[rownames(mat) == tail(from, 1), ]
+        dest_options = mat[rownames(mat) == from, ]
         dest_options = names(dest_options)[ dest_options > 0 ]
         dest_ind = which(dest_options > 0)
         if (length(dest_options) == 0) {
-            if (tail(from, 1) == 'end') {
-                return(from) # Returns the collected from values
+            if (from == 'end') {
+                return(1)
             } else {
                 return(NULL)
             }
-
         }
         out = lapply(dest_options, function(to) {
             if (is_lower(to)){
+                # On visit, add back the index
                 small_cave_visited[small_caves == to] = small_cave_visited[small_caves == to] + 1
                 if (any(small_cave_visited > 1)) {
+                    # If any small cave visited twice, indicate which cave was exceeded.
                     exceeded = names(small_cave_visited)[small_cave_visited > 0]
                     # can't visit that cave any more
                     mat[, colnames(mat) %in% exceeded] = 0
@@ -92,8 +82,8 @@ visit2 = function(mat, from, depth = 0) {
             } else {
                 mat[, colnames(mat) == to] = mat[, colnames(mat) == to] - 1
             }
-            from_collect = c(from, to)
-            .visit(mat, from_collect, depth = depth + 1, small_cave_visited)
+            # from_collect = c(from, to)
+            .visit(mat, to, depth = depth + 1, small_cave_visited)
         })
         return(out)
     }
@@ -101,5 +91,5 @@ visit2 = function(mat, from, depth = 0) {
 }
 
 opts = visit2(adj, 'start')
+cat("Second solution is", sum(unlist(opts)), "\n")
 
-sum(rapply(opts, function(x) 1L))
