5298cd92fce458f46b1fe0a7e9bd3ca9318d31e0
Alec Wong <aw685@cornell.edu>
2022-12-08 23:58:06 -0500

    Update changes with chart

--- 2022/day_01/index.js
+++ 2022/day_01/index.js
@@ -46,21 +46,23 @@ function analyze_1 (input) {
 analyze_1(data)
 
 console.log(elves)
-console.log(`Max value is ${maxval}`)
-console.log(`Elf with max value is ${maxid}`)
+var maxval_statement = `Max value is ${maxval.toLocaleString('en-US')}`
+console.log(maxval_statement)
+var maxid_statement = `Elf with max value is ${maxid}`
+console.log(maxid_statement)
 
 var div_input_data = document.getElementById("input-data-1")
 div_input_data.appendChild(document.createElement('pre')).append(datalines.join("\n"))
 
 var answer_div = document.getElementById("output-1")
-answer_div.appendChild(document.createElement('pre')).append(maxval + "\n" + maxid)
+answer_div.appendChild(document.createElement('pre')).append(maxval_statement + "\n" + maxid_statement)
 
 // Analyze -- Part II
 
 fetch(readme)
     .then(response => response.text())
     .then(txt => {
-        let part1 = "# Part Two" + txt.split("# Part Two")[1]
+        let part1 = "# Part 2" + txt.split("# Part Two")[1]
         document.getElementById('problem2').innerHTML = marked.parse(part1)
     })
 
@@ -72,13 +74,167 @@ function analyze_2 () {
         .sort((a, b) => elves.elf_calories[a] > elves.elf_calories[b] ? -1 : (elves.elf_calories[b] > elves.elf_calories[a]) | 0)
 
     sum = elves.elf_calories[order[0]] + elves.elf_calories[order[1]] + elves.elf_calories[order[2]]
-    console.log(order)
-    console.log(elves.elf_calories[order[0]])
-    console.log(elves.elf_calories[order[1]])
-    console.log(elves.elf_calories[order[2]])
-    console.log(sum)
-
 }
 
 analyze_2()
 
+var sum_statement = `The top three elves are carrying ${(sum).toLocaleString('en-US')} calories.`
+console.log(sum_statement)
+
+var answer2_div = document.getElementById("output-2")
+answer2_div.appendChild(document.createElement('pre')).append(sum_statement)
+
+// Develop graphics for part I and II
+// Want a bar chart that sorts by elf ID first, then by value for the second answer.
+
+// sample newdata
+// var elves = {"elf_id": [1,2,3], "elf_calories": [100, 200, 300], "elf_items": [10, 20, 30]}
+
+function reshape(data) {
+    // Restructure elves data; need one record per row
+    let newdata = []
+    for (let i = 0; i < data.elf_id.length; i++) {
+        let id = data.elf_id[i]
+        let cal = data.elf_calories[i]
+        let items = data.elf_items[i]
+        newdata[i] = { "elf_id": id, "elf_calories": cal, "elf_items": items }
+    }
+    return(newdata)
+}
+
+
+// set the dimensions and margins of the graph
+const margin = { top: 30, right: 30, bottom: 70, left: 60 },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+const x = d3.scaleBand()
+    .range([0, width])
+    .padding(0.2)
+
+const y = d3.scaleLinear()
+    .range([height, 0])
+
+function plot(d) {
+
+    d3.select("svg").remove()
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")")
+        .attr("id", "mainbox")
+
+
+    x.domain(d.map(function (d) { return d.elf_id; }));
+    y.domain([0, d3.max(d, function (d) { return d.elf_calories + 20000; })]);
+
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(d)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) { return x(d.elf_id); })
+        .attr("width", x.bandwidth())
+        .attr("y", function (d) { return y(d.elf_calories); })
+        .attr("height", function (d) { return height - y(d.elf_calories); });
+
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+}
+
+function annotate1() {
+    // Create an annotation
+    let annotations = [{
+        note: {
+            label: maxval_statement,
+            title: "Answer:"
+        },
+        data: { elf_id: maxid, elf_calories: maxval },
+        dx: -50,
+        dy: -10
+    }]
+
+    let makeAnnotations = d3.annotation()
+        .type(d3.annotationLabel)
+        .accessors({
+            x: d => x(d.elf_id) + margin.left,
+            y: d => y(d.elf_calories) + margin.top
+        })
+        .accessorsInverse({
+            elf_id: d => x.invert(d.x),
+            elf_calories: d => y.invert(d.y)
+        })
+        .annotations(annotations)
+
+    d3.select("svg")
+        .append("g")
+        .attr("class", "annotation-group")
+        .call(makeAnnotations)
+}
+
+function annotate2() {
+    // Create an annotation
+    let annotations = [{
+        note: {
+            label: sum_statement,
+            title: "Answer:"
+        },
+        data: { elf_id: maxid, elf_calories: maxval },
+        dx: -50,
+        dy: -10
+    }]
+
+    let makeAnnotations = d3.annotation()
+        .type(d3.annotationLabel)
+        .accessors({
+            x: d => x(d.elf_id) + margin.left,
+            y: d => y(d.elf_calories) + margin.top
+        })
+        .accessorsInverse({
+            elf_id: d => x.invert(d.x),
+            elf_calories: d => y.invert(d.y)
+        })
+        .annotations(annotations)
+
+    d3.select("svg")
+        .append("g")
+        .attr("class", "annotation-group")
+        .call(makeAnnotations)
+}
+
+function barplot(input) {
+
+    let data = reshape(input)
+
+    plot(data)
+    annotate1()
+
+    d3.select("#answer").on("click", (e,d) => {
+        // Change label to answer 2
+        let button = document.getElementById("answer")
+        let answer1 = button.innerHTML === "Answer 1"
+        button.innerHTML = answer1 ? "Answer 2" : "Answer 1"
+
+        // Make plot
+        if (! answer1) {
+            // Update to answer 1
+            data.sort((a,b) => d3.ascending(a.elf_id, b.elf_id))
+            plot(data)
+            annotate1()
+        } else {
+            // Update to answer 2
+            data.sort((a,b) => d3.ascending(a.elf_calories, b.elf_calories))
+            plot(data)
+            annotate2()
+
+        }
+
+    })
+
+}
+
+barplot(elves)
